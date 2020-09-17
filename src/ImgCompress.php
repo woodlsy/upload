c0e4307b7880dd86f205f8044b9f7a05fd41fe50
yls <yelinsen@mayitanlu.cn>
2020-09-17 15:14:41 +0800

    压缩图片

---
+++ src/ImgCompress.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace woodlsy\upload;
+
+class ImgCompress
+{
+    private $src;
+    private $image;
+    private $imageInfo;
+    private $percent = 0.5;
+
+    /**
+     * @var int 画布宽度
+     */
+    private $canvasWidth;
+
+    /**
+     * @var int 画布高度
+     */
+    private $canvasHeight;
+
+
+    /**
+     * @author yls
+     * ImgCompress constructor.
+     * @param     $src
+     */
+    public function __construct($src)
+    {
+        $this->src = $src;
+        list($width, $height, $type, $attr) = getimagesize($this->src);
+        $this->imageInfo = array(
+            'width'  => $width,
+            'height' => $height,
+            'type'   => image_type_to_extension($type, false),
+            'attr'   => $attr
+        );
+
+    }
+
+    /**
+     * 压缩图片
+     *
+     * @author yls
+     * @param int  $width 当auto=false时，为等比例压缩值，小于等于1。否则为画布宽度
+     * @param int  $height 画布高度
+     * @param bool $auto 为false时，固定宽度和高度缩放；
+     *                   为true时是自动缩放，width为0时，以固定高度等比例缩放；
+     *                   height为0时，以固定宽度等比例缩放；
+     *                  都不为0时，以原图像最长的一边固定为width或height，等比例缩放（即width和height构成一盒子，新图像最长的一边不能超出盒子）；
+     * @return $this
+     */
+    public function compressImg($width, $height = 0, $auto = false)
+    {
+        if (true === $auto) { // 自动缩放
+            if (empty($width)) {
+                $percent            = round($height / $this->imageInfo['height'], 2);
+                $this->canvasWidth  = (int) ($this->imageInfo['width'] * $percent);
+                $this->canvasHeight = $height;
+            }elseif (empty($height)) {
+                $percent            = round($width / $this->imageInfo['width'], 2);
+                $this->canvasHeight  = (int) ($this->imageInfo['height'] * $percent);
+                $this->canvasWidth = $width;
+            } else {
+                if ($width > $height) {
+                    $percent = round($height / $this->imageInfo['height'], 2);
+                } else {
+                    $percent = round($width / $this->imageInfo['width'], 2);
+                }
+                $this->canvasWidth  = (int) ($this->imageInfo['width'] * $percent);
+                $this->canvasHeight = (int) ($this->imageInfo['height'] * $percent);
+            }
+        } elseif (!empty($height)) { // 按固定宽高缩放
+            $this->canvasWidth  = $width;
+            $this->canvasHeight = $height;
+        } else { // 按百分比缩放
+            $this->percent = $width;
+        }
+        $this->_openImage();
+        return $this;
+    }
+
+    /**
+     * 保存图像为文件
+     *
+     * @author yls
+     * @param $saveName
+     * @return bool
+     */
+    public function saveImg($saveName)
+    {
+        return $this->_saveImage($saveName);
+    }
+
+    /**
+     * 输出图像
+     *
+     * @author yls
+     */
+    public function outImg()
+    {
+        $this->_showImage();
+    }
+
+    /**
+     * 内部：打开图片
+     */
+    private function _openImage()
+    {
+
+        $fun         = "imagecreatefrom" . $this->imageInfo['type'];
+        $this->image = $fun($this->src);
+        $this->_thumpImage();
+    }
+
+    /**
+     * 内部：操作图片
+     */
+    private function _thumpImage()
+    {
+        $image_thump = imagecreatetruecolor($this->canvasWidth, $this->canvasHeight);
+        //将原图复制带图片载体上面，并且按照一定比例压缩,极大的保持了清晰度
+        imagecopyresampled($image_thump, $this->image, 0, 0, 0, 0, $this->canvasWidth, $this->canvasHeight, $this->imageInfo['width'], $this->imageInfo['height']);
+        imagedestroy($this->image);
+        $this->image = $image_thump;
+    }
+
+    /**
+     * 输出图片:保存图片则用saveImage()
+     */
+    private function _showImage()
+    {
+        header('Content-Type: image/' . $this->imageInfo['type']);
+        $funcs = "image" . $this->imageInfo['type'];
+        $funcs($this->image);
+    }
+
+    /**
+     * 保存图片到硬盘
+     *
+     * @author yls
+     * @param $dstImgName 1、可指定字符串不带后缀的名称，使用源图扩展名 。2、直接指定目标图片名带扩展名。
+     * @return bool
+     */
+    private function _saveImage($dstImgName)
+    {
+        if (empty($dstImgName))
+            return false;
+        $allowImgs = ['.jpg', '.jpeg', '.png', '.bmp', '.wbmp', '.gif'];   //如果目标图片名有后缀就用目标图片扩展名 后缀，如果没有，则用源图的扩展名
+        $dstExt    = strrchr($dstImgName, ".");
+        $sourseExt = strrchr($this->src, ".");
+        if (!empty($dstExt))
+            $dstExt = strtolower($dstExt);
+        if (!empty($sourseExt))
+            $sourseExt = strtolower($sourseExt);
+
+        //有指定目标名扩展名
+        if (!empty($dstExt) && in_array($dstExt, $allowImgs)) {
+            $dstName = $dstImgName;
+        } elseif (!empty($sourseExt) && in_array($sourseExt, $allowImgs)) {
+            $dstName = $dstImgName . $sourseExt;
+        } else {
+            $dstName = $dstImgName . $this->imageInfo['type'];
+        }
+        $funcs = "image" . $this->imageInfo['type'];
+        return $funcs($this->image, $dstName);
+    }
+
+    /**
+     * 销毁图片
+     */
+    public function __destruct()
+    {
+        imagedestroy($this->image);
+    }
+
+}
--- src/Upload.php
+++ src/Upload.php
@@ -1,64 +1,99 @@
 <?php
+
 namespace woodlsy\upload;
 
 use Exception;
 
-class Upload {
-    
+class Upload
+{
+
     public $type = 'image';
-    
+
     public $fieldName = 'file';
-    
+
     public $maxSize = '10M';
-    
+
     protected static $units = array(
         'b' => 1,
         'k' => 1024,
         'm' => 1048576,
         'g' => 1073741824
     );
-    
+
     public $path;
+    /**
+     * @var string 上传后新文件名
+     */
     public $newFileName;
-    
+
     public $fileInfo;
+    /**
+     * @var string 原文件名
+     */
     public $fileName;
     public $fileType;
     public $fileError;
     public $fileSize;
     public $fileTmpName;
-    
+    /**
+     * @var bool 是否压缩
+     */
+    public $compress       = false;
+    public $compressWidth  = 0; // 压缩后宽度
+    public $compressHeight = 0; // 压缩后高度
+    public $compressAuto   = false; // 是否自动压缩
+
     /**
      * 远程上传服务器地址
+     *
      * @var string
      */
     public $serverUrl = null;
-    
+
     public function __construct()
     {
-        
+
     }
-    
+
     public function setFieldName($fieldName)
     {
         $this->fieldName = $fieldName;
         return $this;
     }
-    
+
     public function setMaxSize($size)
     {
         $this->maxSize = $size;
         return $this;
     }
-    
+
+    /**
+     * 压缩图像
+     *
+     * @author yls
+     * @param      $width
+     * @param      $height
+     * @param bool $auto
+     * @return $this
+     */
+    public function compressImg($width, $height, $auto = false)
+    {
+        $this->compress       = true;
+        $this->compressWidth  = $width;
+        $this->compressHeight = $height;
+        $this->compressAuto   = $auto;
+        return $this;
+    }
+
     public function setServerUrl($serverUrl)
     {
         $this->serverUrl = $serverUrl;
-        if(!$this->path)$this->path = '/tmp';
+        if (!$this->path)
+            $this->path = '/tmp';
         return $this;
     }
-    
-    public function Upload($fileName=null)
+
+    public function Upload($fileName = null)
     {
         $this->drawFileInfo();
         $this->checkFileError();
@@ -66,25 +101,32 @@ class Upload {
         $this->checkPath();
         $this->checkFileType();
         $this->newFileName = $this->getFileName($fileName);
-        if(file_exists($this->path.$this->newFileName)){
+        if (file_exists($this->path . $this->newFileName)) {
             throw new Exception('该文件名已存在');
         }
-        
-        if(!move_uploaded_file($this->fileTmpName, $this->path.$this->newFileName)){
-            throw new Exception('文件上传失败');
+
+        if (true === $this->compress) {
+            if (!(new ImgCompress($this->fileTmpName))->compressImg($this->compressWidth, $this->compressHeight, $this->compressAuto)
+                ->saveImg($this->path . $this->newFileName)) {
+                throw new Exception('文件压缩失败');
+            }
+        } else {
+            if (!move_uploaded_file($this->fileTmpName, $this->path . $this->newFileName)) {
+                throw new Exception('文件上传失败');
+            }
         }
-        
-        if($this->serverUrl !== null){
+
+        if ($this->serverUrl !== null) {
             return $this->remoteUpload();
         }
-        
-        $data = [];
+
+        $data          = [];
         $data['name']  = $this->newFileName;
         $data['title'] = $this->fileName;
-        $data['url']   = $this->path.$this->newFileName;
+        $data['url']   = $this->path . $this->newFileName;
         $data['size']  = $this->fileSize;
         $data['type']  = $this->fileType;
-        
+
         return $data;
     }
 
@@ -98,68 +140,66 @@ class Upload {
      */
     public function urlUpload($url, $fileName = null)
     {
-        $imgData = file_get_contents($url);
-        $urlArr = explode('/', $url);
-        $this->fileName = end($urlArr);
-        $this->fileName = current(explode('?', $this->fileName));
-        $this->fileTmpName = '/tmp/'.$this->fileName;
-        $fp = @fopen($this->fileTmpName, 'w');
+        $imgData           = file_get_contents($url);
+        $urlArr            = explode('/', $url);
+        $this->fileName    = end($urlArr);
+        $this->fileName    = current(explode('?', $this->fileName));
+        $this->fileTmpName = '/tmp/' . $this->fileName;
+        $fp                = @fopen($this->fileTmpName, 'w');
         @fwrite($fp, $imgData);
         fclose($fp);
 
         $this->newFileName = $this->getFileName($fileName);
-        if(!copy($this->fileTmpName, $this->path.'/'.$this->newFileName)){
+        if (!copy($this->fileTmpName, $this->path . '/' . $this->newFileName)) {
             throw new Exception('文件上传失败');
         }
         unlink($this->fileTmpName);
 
-        $data = [];
-        $data['url']   = $this->path.$this->newFileName;
+        $data        = [];
+        $data['url'] = $this->path . $this->newFileName;
 
         return $data;
     }
-    
+
     /**
      * 远程上传
-     * 
      * $result = json_encode(array('code'=>0, 'msg'=>'成功', 'name'=>...))
-     * 
-     * @throws Exception
+     *
      * @return mixed
+     * @throws Exception
      */
     private function remoteUpload()
     {
         $curl = curl_init();
-        $data = array('file'=>new \CURLFile(realpath($this->path.$this->newFileName), $this->fileType, $this->fileName));
+        $data = array('file' => new \CURLFile(realpath($this->path . $this->newFileName), $this->fileType, $this->fileName));
         curl_setopt($curl, CURLOPT_URL, $this->serverUrl);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         $result = curl_exec($curl);
-        if(curl_errno($curl))
-        {
+        if (curl_errno($curl)) {
             $errorMsg = curl_error($curl);
             curl_close($curl);
             throw new Exception($errorMsg);
         }
         curl_close($curl);
         $res = @json_decode($result, true);
-        if(!isset($res['code'])){
-            throw new Exception('远程上传返回数据格式不对'.$result);
+        if (!isset($res['code'])) {
+            throw new Exception('远程上传返回数据格式不对' . $result);
         }
-        
-        if($res['code'] != 0){
+
+        if ($res['code'] != 0) {
             throw new Exception($res['msg']);
         }
-        
+
         unset($res['code'], $res['msg']);
-        @unlink($this->path.$this->newFileName);
+        @unlink($this->path . $this->newFileName);
         return $res;
     }
-    
+
     private function drawFileInfo()
     {
-        if(!isset($_FILES[$this->fieldName])){
+        if (!isset($_FILES[$this->fieldName])) {
             throw new Exception('上传文件不存在！');
         }
         $this->fileInfo    = $_FILES[$this->fieldName];
@@ -208,109 +248,114 @@ class Upload {
                 $message = "File upload stopped by extension";
                 throw new Exception($message);
                 break;
-        
+
             default:
                 $message = "Unknown upload error";
                 return true;
                 break;
         }
     }
-    
+
     private function checkFileSize()
     {
         $size = $this->getSizeByte();
-        if($this->fileSize > $size){
+        if ($this->fileSize > $size) {
             throw new Exception("文件大小不能大于{$this->maxSize}");
         }
     }
-    
+
     private function getSizeByte()
     {
-        $value = (int)$this->maxSize;
-        $unit = strtolower(substr($this->maxSize, -1));
+        $value = (int) $this->maxSize;
+        $unit  = strtolower(substr($this->maxSize, -1));
         if (isset(self::$units[$unit])) {
             $value = $value * self::$units[$unit];
         }
         return $value;
     }
-    
+
     private function checkPath()
     {
-        if(!$this->path){
+        if (!$this->path) {
             throw new Exception("未设置上传路径");
         }
-        
-        if(substr($this->path, -1) == '/' || substr($this->path, -1) == '\\'){
+
+        if (substr($this->path, -1) == '/' || substr($this->path, -1) == '\\') {
             $this->path = substr($this->path, 0, -1);
         }
-        
-        if(!is_dir($this->path)){
+
+        if (!is_dir($this->path)) {
             self::directory($this->path);
         }
-        
-        if(!is_dir($this->path)){
-            throw new Exception("上传目录创建失败：".$this->path);
+
+        if (!is_dir($this->path)) {
+            throw new Exception("上传目录创建失败：" . $this->path);
         }
-        
-        if(!is_writable($this->path)){
+
+        if (!is_writable($this->path)) {
             throw new Exception("上传目录不可写入");
         }
-        
-        $this->path = $this->path.'/';
+
+        $this->path = $this->path . '/';
     }
-    
+
     public function setUploadPath($path)
     {
         $this->path = $path;
         return $this;
     }
-    
+
     private function getFileName($filename)
     {
-        if(!$filename)return $this->createFileName();
-        if(strpos('.', $filename) > 0){
+        if (!$filename)
+            return $this->createFileName();
+        if (strpos('.', $filename) > 0) {
             return $filename;
         }
-        return $filename.'.'.$this->getFileNameSuffix();
+        return $filename . '.' . $this->getFileNameSuffix();
     }
-    
+
     private function createFileName()
     {
-        do{
-            $newFileName = $this->randomString().'.'.$this->getFileNameSuffix();
-        }while(file_exists($this->path.$newFileName));
-        
+        do {
+            $newFileName = $this->randomString() . '.' . $this->getFileNameSuffix();
+        } while (file_exists($this->path . $newFileName));
+
         return $newFileName;
     }
-    
+
     private function randomString()
     {
-        $string = time().mt_rand(0, 9999);
+        $string = time() . mt_rand(0, 9999);
         return $string;
     }
-    
+
     private function getFileNameSuffix()
     {
         $suffixArr = explode('.', $this->fileName);
         return end($suffixArr);
     }
-    
+
     private function checkFileType()
     {
-        if($this->type == 'image'){
+        if ($this->type == 'image') {
             $fileTypeArr = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif', 'image/jpg'];
-            if(!in_array($this->fileType, $fileTypeArr)){
-                throw new Exception("非法文件类型");
-            }
+        } elseif ($this->type == 'video') {
+            $fileTypeArr = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif', 'image/jpg'];
+        }
+        if (!empty($fileTypeArr) && !in_array($this->fileType, $fileTypeArr)) {
+            throw new Exception("非法文件类型");
         }
     }
-    
+
     /**
      * 自动创建目录
+     *
      * @param string $dir
      * @return boolean
      */
-    public static function directory( $dir ){
-        return  is_dir ( $dir ) or self::directory(dirname( $dir )) and  @mkdir ( $dir , 0777);
+    public static function directory($dir)
+    {
+        return is_dir($dir) or self::directory(dirname($dir)) and @mkdir($dir, 0777);
     }
 }
